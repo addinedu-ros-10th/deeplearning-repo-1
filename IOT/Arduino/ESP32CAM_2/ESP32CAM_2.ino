901efc5692d59880363d9d741aa0787e640694a6
hwarite <rhkdals1369@gmail.com>
2025-09-19 20:00:40 +0900

    Arduino ESP32 Frirmware Update

--- IOT/Arduino/ESP32CAM_2/ESP32CAM_2.ino
+++ IOT/Arduino/ESP32CAM_2/ESP32CAM_2.ino
@@ -12,7 +12,7 @@
 const char *ssid = "AIE_509_2.4G";
 const char *password = "addinedu_class1";
 
-IPAddress local_IP(192, 168, 0, 62);    // ESP32-CAM IP
+IPAddress local_IP(192, 168, 0, 63);    // ESP32-CAM IP
 IPAddress gateway(192, 168, 0, 1);      // 공유기 게이트웨이
 IPAddress subnet(255, 255, 255, 0);     // 서브넷 마스크
 
@@ -23,6 +23,7 @@ void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
+  WiFi.setHostname("Person-A_ESP32_3"); //ESP32_Name setup
 
   if( !WiFi.config( local_IP, gateway, subnet ) )
   {
@@ -44,33 +45,41 @@ void setup() {
   config.pin_pclk = PCLK_GPIO_NUM;
   config.pin_vsync = VSYNC_GPIO_NUM;
   config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+
   //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
   config.fb_count = 1;
 
-  WiFi.setHostname("Person-A_ESP32_2"); //ESP32_Name setup
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+
   //                      for larger pre-allocated frame buffer.
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
+
+  if(config.pixel_format == PIXFORMAT_JPEG){
+
+    if(psramFound()){
+
       config.jpeg_quality = 10;
       config.fb_count = 2;
       config.grab_mode = CAMERA_GRAB_LATEST;
+
     } else {
+
       // Limit the frame size when PSRAM is not available
       config.frame_size = FRAMESIZE_SVGA;
       config.fb_location = CAMERA_FB_IN_DRAM;
     }
   } else {
+
     // Best option for face detection/recognition
     config.frame_size = FRAMESIZE_240X240;
 #if CONFIG_IDF_TARGET_ESP32S3
@@ -79,8 +88,10 @@ void setup() {
   }
 
 #if defined(CAMERA_MODEL_ESP_EYE)
+
   pinMode(13, INPUT_PULLUP);
   pinMode(14, INPUT_PULLUP);
+
 #endif
 
   // camera init
@@ -88,54 +99,61 @@ void setup() {
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
-  }
 
-  sensor_t *s = esp_camera_sensor_get();
+  }
+  sensor_t * s = esp_camera_sensor_get();
+
   // initial sensors are flipped vertically and colors are a bit saturated
   if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);        // flip it back
-    s->set_brightness(s, 1);   // up the brightness just a bit
-    s->set_saturation(s, -2);  // lower the saturation
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+
   }
+
   // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG) {
+  if(config.pixel_format == PIXFORMAT_JPEG){
     s->set_framesize(s, FRAMESIZE_QVGA);
+
   }
 
 #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
   s->set_vflip(s, 1);
   s->set_hmirror(s, 1);
+
 #endif
 
 #if defined(CAMERA_MODEL_ESP32S3_EYE)
   s->set_vflip(s, 1);
+
 #endif
 
-// Setup LED FLash if LED pin is defined in camera_pins.h
-#if defined(LED_GPIO_NUM)
-  setupLedFlash();
-#endif
-
-  WiFi.mode( WIFI_STA );
   WiFi.begin(ssid, password);
   WiFi.setSleep(false);
-
-  Serial.print("WiFi connecting");
   while (WiFi.status() != WL_CONNECTED) {
+
     delay(500);
+
     Serial.print(".");
+
   }
+
   Serial.println("");
   Serial.println("WiFi connected");
-
   startCameraServer();
+
 
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
+
 }
 
+
 void loop() {
+
   // Do nothing. Everything is done in another task by the web server
+
   delay(10000);
+
 }
