bc08030da391e08d323e4b919bac6de5015e39f1
windhover207 <baksa1993@gmail.com>
2025-09-30 19:39:59 +0900

    feat:ADD dl_project_streaming_object_detection_multi, dl_project_streaming_object_detection_hybrid

--- AI/DL/yolo_detection_and_pose/dl_project_streaming_object_detection_hybrid.py
+++ AI/DL/yolo_detection_and_pose/dl_project_streaming_object_detection_hybrid.py
@@ -15,9 +15,8 @@ start_number = 0
 os.makedirs(video_save_path, exist_ok = True)
 """--------------------------------------------------------------------------------"""
 
-model = YOLO("./runs/detect/json_ai_2057_5items_sdj_v1.0/weights/best.pt")
-# model = YOLO("./runs/detect/json_ai_1046_5items_sdj_v2.0/weights/best.pt")
-# model = YOLO("./runs/detect/json_ai_1046_5items_sdj_v1.0/weights/best.pt")
+model1 = YOLO("./runs/detect/json_ai_1046_5items_sdj_v2.0/weights/best.pt")
+model2 = YOLO("./runs/detect/json_ai_2057_5items_sdj_v1.0/weights/best.pt")
 
 for video_idx in video_file_list:
     video_file_path = os.path.join(video_folder_path, video_idx)
@@ -40,8 +39,16 @@ for video_idx in video_file_list:
         if not ret:
             break
 
-        result = model(frame, classes = [0, 80, 81, 82, 83, 84], conf = 0.45)
+        result = model1(frame, classes = [0, 82, 84], conf = 0.45)
 
+        image = list(result)[0].plot()
+
+        result2 = model2(frame, classes = [80, 81, 83], conf = 0.45)
+
+        image2 = list(result2)[0].plot()
+
+        image3 = cv2.addWeighted(image, 0.5, image2, 0.5, 0)
+        
         image = list(result)[0].plot()
 
         cv2.imshow("test", image)
