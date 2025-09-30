3f47dd8d8282355c68b03fe6cedc56169bc8e81d
windhover207 <baksa1993@gmail.com>
2025-09-30 19:37:28 +0900

    feat:ADD dl_project_streaming_object_detection_multi (test)

---
+++ AI/DL/yolo_detection_and_pose/dl_project_streaming_object_detection_multi.py
@@ -0,0 +1,67 @@
+import cv2
+from ultralytics import YOLO
+import os
+
+"""----------------------------- 비디오 폴더 경로 입력 --------------------------------"""
+
+video_folder_path = "/home/dj/Videos/Screencasts"
+video_file_list = os.listdir(video_folder_path)
+video_type = ".mp4"
+
+video_save_path = os.path.join(video_folder_path, "dl_project_save")
+
+start_number = 0
+
+os.makedirs(video_save_path, exist_ok = True)
+"""--------------------------------------------------------------------------------"""
+
+model = YOLO("./runs/detect/json_ai_2057_5items_sdj_v1.0/weights/best.pt")
+# model = YOLO("./runs/detect/json_ai_1046_5items_sdj_v2.0/weights/best.pt")
+# model = YOLO("./runs/detect/json_ai_1046_5items_sdj_v1.0/weights/best.pt")
+
+cap = cv2.VideoCapture(0)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+cap_out = cv2.VideoWriter("output.avi", fourcc, fps, (1280, 960))
+
+frame_count = 0
+
+
+for video_idx in video_file_list:
+    video_file_path = os.path.join(video_folder_path, video_idx)
+
+    cap = cv2.VideoCapture(video_file_path)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    period = 1/fps
+
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+
+    save_file = video_save_path + "/" + str(start_number) + video_idx.split(".")[0] + "_edit" + video_type
+
+    cap_out = cv2.VideoWriter(save_file, fourcc, fps, (1280, 960))
+
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        result = model(frame, classes = [0, 80, 81, 82, 83, 84], conf = 0.45)
+
+        image = list(result)[0].plot()
+
+        cv2.imshow("test", image)
+
+        cap_out.write(frame)
+
+        key_input = cv2.waitKey(1)
+
+        if key_input == ord('q') &0xff:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
